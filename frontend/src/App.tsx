98d2c5f158b3a861f86c7769a225e44bf73c8f3f
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-02-25 02:17:14 +0900

    🚧 experiment component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import './App.css'
+import './App.css';
+import ShiftTable from './components/ShiftTable';
+import { ShiftData, getSundayList } from './components/types';
 
 function App() {
+  const sundayList = getSundayList(2024, 2);
+  const data: ShiftData[] = [
+    new ShiftData("namae1", sundayList.length),
+    new ShiftData("namae2", sundayList.length),
+    new ShiftData("namae3", sundayList.length),
+  ];
 
   return (
     <>
-    hoge
+    <ShiftTable dates={sundayList} shiftData={data} />
     </>
   )
 }
